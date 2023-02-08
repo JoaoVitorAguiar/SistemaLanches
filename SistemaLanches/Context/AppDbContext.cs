541288095f2fdc856e4666f3cfd28740f9acf68c
J. Vitor <joaovitor4guiar@gmail>
2023-02-08 10:03:41 -0300

    comecando a implementacao com EntityFrameworkCore

---
+++ SistemaLanches/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaLanches.Models;
+
+namespace SistemaLanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set;}
+    }
+}

18aca1d5703d64609fab82492c77ea8aafc3e22d
J. Vitor <joaovitor4guiar@gmail>
2023-02-23 19:47:57 -0300

    Criando o Controller e a View para exibir os lanches

---
+++ SistemaLanches/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaLanches.Repositories.Interfaces;
+
+namespace SistemaLanches.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lacheRepository;
+
+        public LancheController(ILancheRepository lacheRepository)
+        {
+            _lacheRepository = lacheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lacheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}

845498508ddde6f1d2d09bae687804b0bbd8cd5c
J. Vitor <joaovitor4guiar@gmail>
2023-02-25 21:00:44 -0300

    adicionar Itens ao Carrinho de Compras

---
+++ SistemaLanches/Models/CarrinhoCompra.cs
@@ -0,0 +1,62 @@
+﻿using SistemaLanches.Context;
+using SistemaLanches.Migrations;
+
+namespace SistemaLanches.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+
+        public void AdicionarCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                    s => s.Lanche.LancheId == lanche.LancheId &&
+                    s.CarrinhoCompraId == CarrinhoCompraId
+                );
+
+            if (carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+            }
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+            _context.SaveChanges();
+        }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider service)
+        {
+            // Define uma seção
+            ISession session = service.GetRequiredService<HttpContextAccessor>()?.HttpContext.Session;
+
+            // Obtem um serviço do tipo do nosso contexto
+            var context = service.GetService<AppDbContext>();
+
+            // Obtem ou gera o Id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            // Atribui o id do carrinho na Sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            // Retorna o carrinho com o contexto e o Id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId,
+            };
+        }
+    }
+}
